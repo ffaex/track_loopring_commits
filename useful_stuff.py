ec8b9d39cab6b9b52a3981e6527eb31d5585f5d2
ffaex <fyodor.fomichev@gmail.com>
2021-11-07 00:51:12 +0100

    write to files function added

--- useful_stuff.py
+++ useful_stuff.py
@@ -3,3 +3,7 @@ import json
 def pretty_response(res):
     pretty_response = json.dumps(res, indent=4)
     return pretty_response
+
+def write_to_file(file_name, data):
+    with open(file_name, 'w') as f:
+        f.write(data)
