4bc2984820c8ab13012bc91fe6b7565b9ee14cd0
ffaex <fyodor.fomichev@gmail.com>
2021-11-07 00:22:17 +0100

    changed main.py and deleted some useful stuff

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print('hello world!')
+from time import sleep
+import requests
+from useful_stuff import pretty_response, write_to_file
+
+
+BASE_URL = 'https://api.github.com'
+ORG = 'Loopring'
+
+def get_repos():
+    repo_list = []
+    url = '{}/orgs/{}/repos'.format(BASE_URL, ORG)
+    request = requests.get(url)
+    json_data =  request.json()
+   
+    for i in json_data:
+        repo_list.append(i["name"])
+    
+    return repo_list
+
+def get_branches(repo):
+    url = '{}/repos/{}/{}/branches'.format(BASE_URL, ORG, repo)
+    request = requests.get(url)
+    return request.json()
+
+def main():
+    repos = get_repos()
+    print(get_branches(repos[-3]))
+
+
+main()
--- useful_stuff.py
+++ useful_stuff.py
@@ -1,9 +1,5 @@
 import json
 
 def pretty_response(res):
-    pretty_response = json.dumps(res.json(), indent=4)
+    pretty_response = json.dumps(res, indent=4)
     return pretty_response
-
-def write_to_file(file_name, data):
-    with open(file_name, 'w') as f:
-        f.write(data)
