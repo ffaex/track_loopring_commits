fad8a53fa8954ef2c6aa08315c27eb516c66029c
ffaex <fyodor.fomichev@gmail.com>
2021-11-06 23:36:06 +0100

    was dumb

--- useful_stuff.py
+++ useful_stuff.py
@@ -4,6 +4,6 @@ def pretty_response(res):
     pretty_response = json.dumps(res.json(), indent=4)
     return pretty_response
 
-def write_to_file(file_name):
+def write_to_file(file_name, data):
     with open(file_name, 'w') as f:
-        f.write(pretty_response)
+        f.write(data)
