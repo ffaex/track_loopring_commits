0a5be858f064763e722b5049f25addf4f5515129
ffaex <fyodor.fomichev@gmail.com>
2021-11-07 20:17:18 +0100

    TODO check if file is text based

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
+from logging import exception
 import re
 import mysql.connector
+import datetime
+from datetime import date, time, datetime
 from time import sleep
 import requests
 from requests.api import request
@@ -42,7 +45,18 @@ def get_commits_shas(repo):
     request = requests.get(url)
     json_data = request.json()
     for i in json_data:
-        shas.append(i['sha'])
+        try:
+            commit = i['sha']
+            tmp = i['commit']['committer']['date']
+            #datetime.datetime.strptime(tmp,"%Y-%m-%dT%H:%M:%SZ")
+            now = datetime.now()
+            pretty_date_delta = datetime.strptime(tmp,"%Y-%m-%dT%H:%M:%SZ") - now
+            if pretty_date_delta.days *(-1) > 30:
+                print('skipped cause too old')
+                continue
+            shas.append(i['sha'])
+        except Exception as e:
+            print(e + 'line 58')
     return shas
 
 def get_commit_urls(repo, commit_sha):
@@ -52,11 +66,15 @@ def get_commit_urls(repo, commit_sha):
     request = requests.get(url)
     commit = request.json()
 
-    files = commit["files"]
-    for dictiniary in files:
-        raw_urls.append(dictiniary["raw_url"])
-    
-    return raw_urls
+    try:
+        files = commit["files"]
+        for dictiniary in files:
+            # TODO check if text based
+            raw_urls.append(dictiniary["raw_url"])
+        
+        return raw_urls
+    except Exception as e:
+        print(e)
 
 def insert_hash(hash, repo):
     sql = "INSERT IGNORE INTO hashes (hash, repo_name) VALUES (%s, %s)"
@@ -86,28 +104,58 @@ def insert_repos(repos):
 
 def check_keywords(url):
     raw_data = requests.get(url).text
+    print('checking keywords, len of string: ' + str(len(raw_data)))
     for i in KEYWORDS:
         if re.search(i, raw_data, re.IGNORECASE):
+            print('done with checking!!!')
             return True
+
+    print('done with checking!!!')
     return False
+
+def check_date_of_commit(repo, sha):
+    url = '{}/repos/{}/{}/commits'.format(BASE_URL, ORG, repo)
+    request = requests.get(url)
+    json_data = request.json()
+    
     pass
 
 
 
 def main():
-    repo = get_repos()[-3]
-    shas = get_commits_shas(repo)
-    #insert_hash(shas[2], repo)
-    urls = get_commit_urls(repo, shas[2])
+    global today
+    today  = date.today()
+    #t0 = time.time()
+    repos = get_repos()
+    for repo in repos:
+        shas = get_commits_shas(repo)
+        for sha in shas:
+            if check_if_hash_exists(repo, sha):
+                print('exists')
+                continue
+            print('new')
+            urls = get_commit_urls(repo, sha)
+            for url in urls:
+                if check_keywords(url):
+                    print('BIG')
+                    print(url)
+    #t1 = time.time()
+    #print('total time: ', t1-t0)
+
+main()
+    # repo = get_repos()[-3]
+    # shas = get_commits_shas(repo)
+    # #insert_hash(shas[2], repo)
+    # urls = get_commit_urls(repo, shas[2])
     
 
-    #write_to_file('raw_urls.json', pretty_response(get_commit_content(urls)))
-    write_to_file('raw_urls.json', pretty_response(urls))
-    #write_to_file('commits.json', pretty_response(shas))
-    #print(get_branches(repos[-3]))
-    url = '{}/repos/{}/{}/commits'.format(BASE_URL, ORG, repo)
-    json_data = requests.get(url).json()
-    write_to_file('commits.json', pretty_response(json_data))
+    # #write_to_file('raw_urls.json', pretty_response(get_commit_content(urls)))
+    # write_to_file('raw_urls.json', pretty_response(urls))
+    # #write_to_file('commits.json', pretty_response(shas))
+    # #print(get_branches(repos[-3]))
+    # url = '{}/repos/{}/{}/commits'.format(BASE_URL, ORG, repo)
+    # json_data = requests.get(url).json()
+    # write_to_file('commits.json', pretty_response(json_data))
 
 
 
@@ -116,7 +164,7 @@ def main():
 #insert_repos(get_repos())
 
 #repos = get_repos()
-print(check_if_hash_exists('whitepaper', '7c0d341aef7c54dd1dae24fe41727e99083bec2e'))
+#print(check_if_hash_exists('whitepaper', '7c0d341aef7c54dd1dae24fe41727e99083bec2e'))
 
 # for repo in repos:
 #     shas = get_commits_shas(repo)
